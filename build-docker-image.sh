41c093348ab5ff7ec8dc75f9b8c5bb2e326805c4
Oliver Gugger <gugger@puzzle.ch>
2017-06-07 16:19:11 +0200

    Add build script

---
+++ build-docker-image.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -ex
+
+NAMESPACE="guggero"
+DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
+
+function build {
+  COMPONENT=$1
+  VERSION=$2
+  echo "Building $COMPONENT with version $VERSION"
+  docker build --build-arg VERSION=$VERSION -t $NAMESPACE/$COMPONENT -t $NAMESPACE/$COMPONENT:latest -t $NAMESPACE/$COMPONENT:$VERSION .
+  docker push $NAMESPACE/$COMPONENT
+  docker push $NAMESPACE/$COMPONENT:latest
+  docker push $NAMESPACE/$COMPONENT:$VERSION
+}
+
+build pivx 2.2.1
